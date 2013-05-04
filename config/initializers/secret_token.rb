14ea1269a02683eff49d9afef2458c5c91b50a1b
Scott Huey <hueysm@gmail.com>
2013-05-04 06:29:27 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tagpup::Application.config.secret_token = '0ccb15a0e210e362ed393ff3b9eb197e791a9ea1dfc07d4664516c55d37cbad50f312412240663be3d42ce34804a846e5717999c0d1d8da030af3c471bebb115'
